8cb4ec30a5ca6422924f8994a29bf113c88d3531
Vonzelle-Cuizon <143152384+Vonzelle-Cuizon@users.noreply.github.com>
2024-09-25 16:08:51 +0800

    Update Hourly.java

--- EmployeeVer2/src/main/java/com/mycompany/employeever2/Hourly.java
+++ EmployeeVer2/src/main/java/com/mycompany/employeever2/Hourly.java
@@ -75,12 +75,14 @@ public class Hourly extends Employee{
         return this.TotalHoursWorked>40?this.TotalHoursWorked * this.ratePerHour+ ((this.TotalHoursWorked - 40) * this.ratePerHour * .5):this.TotalHoursWorked * this.ratePerHour;
     }
     
-    public void DisplayHourly1(){
-        
+   
+    void display(){
+        System.out.printf("Name: %s\nEMP ID: %d\n Salary: %.2f\n",super.Name,super.empID,computeSalary());
     }
+    
     @Override
     public String toString() {
-        return ("Name " + super.Name + "\nEMP ID " + super.empID + "\nSalary " + computeSalary());
+        return ("Name " + super.Name + "\nEMP ID " + super.empID);
     }
 
     
