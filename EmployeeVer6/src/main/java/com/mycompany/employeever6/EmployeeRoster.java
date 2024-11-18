92d7c3b562df46d0b9f0f2ab12cd0ebe2c186d4c
Vonzelle-Cuizon <143152384+Vonzelle-Cuizon@users.noreply.github.com>
2024-11-18 15:30:27 +0800

    Add files via upload

---
+++ EmployeeVer6/src/main/java/com/mycompany/employeever6/EmployeeRoster.java
@@ -0,0 +1,170 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.employeever6;
+import java.util.ArrayList;
+/**
+ *
+ * @author User
+ */
+public class EmployeeRoster {
+    public ArrayList<Employee> emplist;
+
+    public EmployeeRoster(ArrayList<Employee> emplist) {
+        this.emplist = emplist;
+    }
+
+    public ArrayList<Employee> getEmplist() {
+        return emplist;
+    }
+
+    public void setEmplist(ArrayList<Employee> emplist) {
+        this.emplist = emplist;
+    }
+
+    
+   
+    
+    public boolean addEmployee(Employee emp){
+       for(int i=0;i<emplist.size();i++){
+            if(emplist.get(i).empID == emp.empID){
+                System.out.println(emp.empID + " - " + emp.empName +"  Already added to lsit");
+                return false;
+            }
+       }
+        
+      emplist.add(emp);
+      return true;
+    }
+    
+    public Employee removeEmployee(int id){
+        Employee temp = null;
+        for(int i=0;i<emplist.size();i++){
+            if(emplist.get(i).equals(id)){
+                temp = emplist.get(i);
+                emplist.remove(i);
+                break;
+            }
+        }
+        
+        return temp;
+    }
+    
+    public int countHE(){
+        int x = 0;
+        for(int i=0;i<emplist.size();i++){
+            if(emplist.get(i) instanceof Hourly)x++;
+        }
+        
+        return x;
+    }
+    
+    public int countCE(){
+        int x = 0;
+        for(int i=0;i<emplist.size();i++){
+            if(emplist.get(i) instanceof Comm && !(emplist.get(i) instanceof Based))x++;
+        }
+        
+        return x;
+    }
+    
+    public int countBCPE(){
+        int x = 0;
+        for(int i=0;i<emplist.size();i++){
+            if(emplist.get(i) instanceof Based)x++;
+        }
+        
+        return x;
+    }
+    
+    public int countPWE(){
+        int x = 0;
+        for(int i=0;i<emplist.size();i++){
+            if(emplist.get(i) instanceof Piece)x++;
+        }
+        
+        return x;
+    }
+    
+    public void displayHE(){
+        System.out.println("Hourly Employees: ");
+        for(int i=0;i<emplist.size();i++){
+            if(emplist.get(i) instanceof Hourly){
+                
+                emplist.get(i).display();
+            }
+        }
+        System.out.println("-------------------------");
+    }
+    
+    public void displayCE(){
+        System.out.println("Commission Employees: ");
+        for(int i=0;i<emplist.size();i++){
+            if(emplist.get(i) instanceof Comm && !(emplist.get(i) instanceof Based)){
+                
+                emplist.get(i).display();
+            }
+        }
+        
+        System.out.println("-------------------------");
+    }
+    
+    public void displayBCPE(){
+        System.out.println("Based Employees: ");
+        for(int i=0;i<emplist.size();i++){
+            if(emplist.get(i) instanceof Based){
+                
+                emplist.get(i).display();
+            }
+        }
+        System.out.println("-------------------------");
+    }
+    
+    public void displayPWE(){
+        System.out.println("Piece Employees: ");
+        for(int i=0;i<emplist.size();i++){
+            if(emplist.get(i) instanceof Piece){
+                
+                emplist.get(i).display();
+            }
+        }
+        System.out.println("-------------------------");
+    }
+    
+    public void displayAll(){
+        System.out.println("ID    |          Name          |   Date Hired   |    Salary    |  Type     " );
+        
+        for (int i=0; i<emplist.size(); i++) {
+            double salary = 0.0;
+            if(emplist.get(i) instanceof Hourly){
+                Hourly hEmp = (Hourly)emplist.get(i);
+                salary = hEmp.computeSalary();
+            }
+            
+            if(emplist.get(i) instanceof Piece){
+                Piece pEmp = (Piece)emplist.get(i);
+                salary = pEmp.computeSalary();
+            }
+            
+            if(emplist.get(i) instanceof Based){
+                Based bEmp = (Based)emplist.get(i);
+                salary = bEmp.computeSalary();
+            }
+            
+            if(emplist.get(i) instanceof Comm && !(emplist.get(i) instanceof Based)){
+                Comm cEmp = (Comm)emplist.get(i);
+                salary = cEmp.computeSalary();
+            }
+            
+            System.out.printf("%-5d | %-22s | %-14s | %-12.2f | %-12s\n", 
+                emplist.get(i).getEmpID(),        
+                emplist.get(i).getEmpName(),      
+                emplist.get(i).getDateHired(),    
+                salary, 
+                emplist.get(i).getClass().getSimpleName()
+            );
+        }
+    }
+    
+}
---
+++ EmployeeVer6/src/main/java/com/mycompany/employeever6/EmployeeVer6.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.employeever6;
+import java.util.ArrayList;
+/**
+ *
+ * @author User
+ */
+public class EmployeeVer6 {
+
+    public static void main(String[] args) {
+        ArrayList<Employee> employees = new ArrayList<>();
+        EmployeeRoster roster = new EmployeeRoster(employees);
+        
+        Hourly emp1 = new Hourly(10,10,"Vonzelle","Devila","Cuizon",1,15,6,2005);
+        roster.addEmployee(emp1);
+        
+        Hourly Anna = new Hourly(10,10,"Anna","Lynn","Baro",69,12,12,2005);
+        roster.addEmployee(Anna);
+        
+        Piece emp2 = new Piece(10,10,"Jesus","Mary","joseph",2,15,6,2005);
+        roster.addEmployee(emp2);
+        
+        Comm emp3 = new Comm(10000,"Geogre","George","geroge",3,15,6,2005);
+        roster.addEmployee(emp3);
+        
+        Based emp4 = new Based(1000,10000,"Geogre","George","geroge",4,15,6,2005);
+        roster.addEmployee(emp4);
+        
+        Based emp5 = new Based(1000,10000,"Geogre","George","geroge",5,15,6,2005);
+        roster.addEmployee(emp5);
+        
+        Comm emp6 = new Comm(10000,"Geogre","George","geroge",6,15,6,2005);
+        roster.addEmployee(emp6);
+        
+        Piece emp7 = new Piece(100,100,"Santa","Cricket","Christ",7,1,2,2003);
+        roster.addEmployee(emp7);
+        
+        
+//        System.out.println(roster.countHE());
+//        System.out.println(roster.countBCPE());
+//        System.out.println(roster.countCE());
+//        System.out.println(roster.countPWE());
+//        
+//        roster.removeEmployee(2);
+//        System.out.println("--");
+//        
+//        System.out.println(roster.countHE());
+//        System.out.println(roster.countBCPE());
+//        System.out.println(roster.countCE());
+//        System.out.println(roster.countPWE());
+        
+        roster.displayHE();
+        
+        roster.displayPWE();
+         
+        roster.displayCE();
+        
+        roster.displayBCPE();
+        
+        System.out.println(" ");
+        
+        roster.displayAll();
+    }
+}
